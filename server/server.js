8916e9f550b91f7c1d9676bf6802fd131cb5e528
Rohan Ardhapure <rohanardhapure83@gmail.com>
2024-01-29 22:54:52 +0530

    updated server start

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const connectDB = async () =>{
    }
 }
 connectDB();
-app.use(cors());
+app.use(cors({
+   credentials:true,
+   origin:'https://jwellerybackend.onrender.com/'
+}));
 app.use(express.json());
 app.use('/',require('./routes/authRoutes'));
 const port=8000;
