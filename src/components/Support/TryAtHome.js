57e62cd1bfd85af57eb47feb361bdfb7da8d56f3
Rohan Ardhapure <rohanardhapure83@gmail.com>
2024-02-02 22:38:44 +0530

    updated server start

--- src/components/Support/TryAtHome.js
+++ src/components/Support/TryAtHome.js
@@ -123,14 +123,11 @@ export class TryAtHome extends Component {
                                 <div className='col-md-3 col-12 product-page'>
                                     <div className=' product-image'>
                                         {ProductImages[item.images] ? <div className='image-holder'><img className='w-100' src={ProductImages[item.images]} alt={item.description} /></div> : null}
-                                        <p className='item-quantity'><FaMinus onClick={(e) => this.props.subtractQuantity(item.name)}></FaMinus><input type='number' value={item.quantity} className='quantity' onChange={(e) => this.changeQuantity(e, item.name)} /><FaPlus onClick={(e) => this.addSingleQuantity(item.name)}></FaPlus> </p>
                                     </div>
                                 </div>
                                 <div className='col-md-9 col-12 product-details'>
                                     <h6 className='product-name'>{item.name}</h6>
                                     <p className='desc-text'>{item.description}</p>
-                                    <h4 className='price'>₹{item.discount ? <span>{this.Offerprice(item.discount, item.price)} <span className='old-price'>{item.price}</span><span className='discount'>-{item.discount}%off</span></span> : <span>{item.price}</span>}</h4>
-                                    <button className='remove-button' onClick={(e) => this.removeItem(e, item.name)}>Remove</button>
                                 </div>
                             </div>
                         </div >
@@ -150,12 +147,7 @@ export class TryAtHome extends Component {
                                 </div>
                             </div>
                             <div className='col-lg-4 col-12'>
-                                <div className='total-details'>
-                                    <h4>Price details</h4>
-                                    <div className='d-flex w-100 mt-4'><h6>Price ({this.props.totalQuantity} items)</h6><h6 className='ms-auto'>₹{this.props.total + this.props.totalDiscount}</h6></div>
-                                    <div className='d-flex mt-4'><h6>discount</h6><h6 className='ms-auto'>-₹{this.props.totalDiscount}</h6></div>
-                                    <div className='d-flex mt-4'><h4>Total Amount</h4><h4 className='ms-auto'>₹{this.props.total}</h4></div>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
