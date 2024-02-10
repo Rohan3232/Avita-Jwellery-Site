596e6d04df2533baf2ad55b0768e1d1c40cd657a
Rohan Ardhapure <rohanardhapure83@gmail.com>
2024-02-10 22:01:57 +0530

    updated server start

--- src/components/reducers/BangleItems.js
+++ src/components/reducers/BangleItems.js
@@ -214,6 +214,7 @@ const BangleItems = (state = initState, action) => {
         }
     }
     if (action.type ===ADD_TO_TRYOUTCART) {
+       var userid=state.userid;
         let addedItem;
         state.items.filter(subtype => subtype.types).map((maintype) => {
             maintype.types.map((subtype) => {
