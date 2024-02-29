24f7435d1689c47c0b23383ed3b2cdf653fb0f67
Rohan Ardhapure <rohanardhapure83@gmail.com>
2024-02-29 22:13:24 +0530

    updated code

--- src/components/Navbar/PrimaryNav.js
+++ src/components/Navbar/PrimaryNav.js
@@ -1,4 +1,4 @@
-import React, { Component, useState} from "react";
+
 import React, { Component, useEffect, useState} from "react";
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
